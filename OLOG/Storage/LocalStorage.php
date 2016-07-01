a4485497e70bba0ffe2611914d68a65b41617e90
mkozlov1 <650465@gmail.com>
2016-07-01 17:39:21 +0300

    1

--- OLOG/Storage/LocalStorage.php
+++ OLOG/Storage/LocalStorage.php
@@ -8,7 +8,7 @@ class LocalStorage
 {
     protected $root_path;
     protected $name;
-    
+
     /**
      * @return string
      */
@@ -41,5 +41,46 @@ class LocalStorage
         $this->name = $name;
     }
 
+    public function putFileToStorage($source_path, $destination_path_in_storage)
+    {
+        $destination_full_path = $this->getRootPath() . $destination_path_in_storage;
+        $destination_directory = dirname($destination_full_path);
 
+        if (!file_exists($destination_directory)) {
+            if (!mkdir($destination_directory, 0777, true)) {
+                $error_arr = error_get_last();
+                throw new \Exception('mkdir ' . $destination_directory . ' error: ' . $error_arr['message']);
+            }
+        }
+
+        if (copy($source_path, $destination_full_path)) {
+            return;
+        }
+
+        $error_arr = error_get_last();
+        throw new \Exception('copy error: ' . $error_arr['message']);
+    }
+
+    public function getFileFromStorage($source_path_in_storage, $destination_path)
+    {
+        $source_full_path = $this->getRootPath() . $source_path_in_storage;
+        \OLOG\Assert::assert(file_exists($source_full_path), 'file ' . $source_full_path . ' not found');
+
+        \OLOG\Assert::assert(is_file($source_full_path), 'file ' . $source_full_path . ' not found or is not file');
+
+        $destination_directory = dirname($destination_path);
+        if (!file_exists($destination_directory)) {
+            if (!mkdir($destination_directory, 0777, true)) {
+                $error_arr = error_get_last();
+                throw new \Exception('mkdir ' . $destination_directory . ' error: ' . $error_arr['message']);
+            }
+        }
+
+        if (copy($source_full_path, $destination_path)) {
+            return;
+        }
+
+        $error_arr = error_get_last();
+        throw new \Exception('copy error: ' . $error_arr['message']);
+    }
 }
