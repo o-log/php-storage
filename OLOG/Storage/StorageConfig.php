2be56ed0cd8fab0b7bcbae6ea06e178f44ec8757
mkozlov1 <650465@gmail.com>
2016-10-11 13:11:25 +0300

    1

--- OLOG/Storage/StorageConfig.php
+++ OLOG/Storage/StorageConfig.php
@@ -8,16 +8,19 @@ class StorageConfig
 {
     static protected $storages_arr = [];
 
-    static public function setStorage($storage_id, $storage_obj){
+    static public function setStorage($storage_id, $storage_obj)
+    {
         self::$storages_arr[$storage_id] = $storage_obj;
     }
 
-    static public function getStoragesArr(){
+    static public function getStoragesArr()
+    {
         return self::$storages_arr;
     }
 
-    static public function getStorageObjById($storage_id){
-        Assert::assert(array_key_exists($storage_id, self::$storages_arr));
+    static public function getStorageObjById($storage_id)
+    {
+        Assert::assert(array_key_exists($storage_id, self::$storages_arr), 'Storage "' . $storage_id . '" not configured');
         return self::$storages_arr[$storage_id];
     }
 }
